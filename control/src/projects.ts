0400da3231832b07a6fc7bc63f5fd9ea1ed297e0
jde-cpp <johnduffyx@outlook.com>
2020-12-19 15:16:44 -0500

    make a control.

--- control/src/projects.ts
+++ control/src/projects.ts
@@ -5,3 +5,16 @@
 export * from './lib/jde-material-site.service';
 export * from './lib/jde-material-site.component';
 export * from './lib/jde-material-site.module';
+export * from './lib/pages/page-title/page-title';
+export * from './lib/pages/component-page-header/component-page-header';
+export * from './lib/pages/component-sidenav/component-sidenav-can-load-guard';
+export * from './lib/shared/footer/footer';
+export * from './lib/shared/navbar/navbar';
+export * from './lib/shared/navigation-focus/navigation-focus';
+export * from './lib/shared/navigation-focus/navigation-focus.service';
+export * from './lib/shared/style-manager/style-manager';
+export * from './lib/shared/svg-viewer/svg-viewer';
+export * from './lib/shared/theme-picker/theme-picker';
+export * from './lib/shared/theme-picker/theme-storage/theme-storage';
+
+
