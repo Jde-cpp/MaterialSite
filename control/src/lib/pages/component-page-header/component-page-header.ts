4939d7486639b7e9ed0dc91cea6480c4fb7d6c5c
jde-cpp <johnduffyx@outlook.com>
2022-05-30 16:39:22 -0400

    add back button to component page header.

--- control/src/lib/pages/component-page-header/component-page-header.ts
+++ control/src/lib/pages/component-page-header/component-page-header.ts
@@ -1,8 +1,10 @@
-import {Component, EventEmitter, NgModule, Output} from '@angular/core';
-import {ComponentPageTitle} from '../page-title/page-title';
-import {NavigationFocusModule} from '../../shared/navigation-focus/navigation-focus';
+import {Component, EventEmitter, Input, NgModule, Output} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
+import {ComponentPageTitle} from '../page-title/page-title';
+import {NavigationFocusModule} from '../../shared/navigation-focus/navigation-focus';
 
 @Component({
   selector: 'component-page-header',
@@ -10,17 +12,21 @@ import {MatIconModule} from '@angular/material/icon';
   styleUrls: ['./component-page-header.scss']
 })
 export class ComponentPageHeader {
-  constructor(public _componentPageTitle: ComponentPageTitle) {}
+  constructor( public _componentPageTitle: ComponentPageTitle, private router: Router )
+	{
+	}
 
   @Output() toggleSidenav = new EventEmitter<void>();
 
   getTitle() {
     return this._componentPageTitle.title;
   }
+  @Input()backUrl:string;
+  back(){ this.router.navigate([this.backUrl] ); }
 }
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, NavigationFocusModule],
+  imports: [MatButtonModule, MatIconModule, NavigationFocusModule, CommonModule],
   exports: [ComponentPageHeader],
   declarations: [ComponentPageHeader],
 })
