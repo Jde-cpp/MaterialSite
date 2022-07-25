a699b9c44ee51162782b02ffbe3df00e14c62b22
jde-cpp <johnduffyx@outlook.com>
2022-07-25 09:20:31 -0400

    fix build.

--- control/src/lib/shared/navbar/navbar.ts
+++ control/src/lib/shared/navbar/navbar.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {Router, RouterModule} from '@angular/router';
-import {ThemePickerModule} from '../theme-picker';
+import {ThemePickerModule} from '../theme-picker/theme-picker';
 import {SECTIONS} from '../documentation-items/documentation-items';
 import {ThemeStorage} from '../theme-picker/theme-storage/theme-storage';
 import {StyleManager} from '../style-manager';
